72343da23ff7359fa6d12e0ec1e7a154da6d7d7b
Stealth-py <generalom1234@gmail.com>
2021-06-08 22:16:20 +0530

    hmm

--- bot.py
+++ bot.py
@@ -32,16 +32,36 @@ async def coding(ctx):
 @bot.command()
 async def updatelb(ctx):
     msg = ctx.message.content.strip(".updatelb ")
-    usr, cod, cr = msg.split()
+    usr, code, crypt = msg.split()
+    code = int(code)
+    crypt = int(crypt)
+
+    print(usr, code, crypt)
 
     compltlb = {}
     with open("assets/complete_leaderboard.json", "r") as f:
         compltlb = json.load(f)
     f.close()
 
+    if not compltlb:
+        compltlb = {usr: {"code": code, "crypt": crypt}}
+    else:
+        if usr in compltlb:
+            compltlb[usr]["code"]+=code
+            compltlb[usr]["crypt"]+=crypt
+        else:
+            compltlb[usr] = {"code": code, "crypt": crypt}
+    
+    obj = json.dumps(compltlb, indent = 4)
+    print(obj)
+    with open("assets/complete_leaderboard.json", "w") as f:
+        f.write(obj)
+
     wklylb = {}
-    with open("assets/weekly_leaderboard", "r") as f:
+    with open("assets/weekly_leaderboard.json", "r") as f:
         wklylb = json.load(f)
     f.close()
+
+    
 
 bot.run(TOKEN)
