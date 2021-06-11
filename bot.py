56822312a187e10c48a21d67e6e35d87bb373ee1
Stealth-py <generalom1234@gmail.com>
2021-06-12 02:23:45 +0530

    hmm

--- bot.py
+++ bot.py
@@ -2,16 +2,32 @@ import discord, os
 from discord.ext import commands
 from dotenv import load_dotenv
 import json
+from datetime import datetime
+import calendar
 
 load_dotenv()
-
 TOKEN = os.environ.get("BOT_TOKEN")
 
 cryptic_id = os.environ.get("CRYPTIC")
 coding_id = os.environ.get("CODING")
+lb_id = os.environ.get("LEADERBOARD_CHANNEL")
 
 bot = commands.Bot(command_prefix=".", case_insensitive = True)
 bot.remove_command('help')
+
+########
+#change weekly lb on monday 00:00 and print
+
+@bot.event()
+async def on_time():
+    my_date = datetime.utcnow()
+    day = calendar.day_name[my_date.weekday()]
+    if "00:00:00" in my_date and day=="Monday":
+        ch = bot.get_channel(lb_id)
+        emb1 = discord.Embed(title = "Weekly Leaderboard", color = 0xFFD700)
+
+
+########
 
 @bot.command()
 async def cryptic(ctx):
@@ -36,8 +52,6 @@ async def updatelb(ctx):
     code = int(code)
     crypt = int(crypt)
 
-    print(usr, code, crypt)
-
     compltlb = {}
     with open("assets/complete_leaderboard.json", "r") as f:
         compltlb = json.load(f)
@@ -56,12 +70,25 @@ async def updatelb(ctx):
     print(obj)
     with open("assets/complete_leaderboard.json", "w") as f:
         f.write(obj)
+    f.close()
 
     wklylb = {}
     with open("assets/weekly_leaderboard.json", "r") as f:
         wklylb = json.load(f)
     f.close()
-
+    if not wklylb:
+        wklylb = {usr: {"code": code, "crypt": crypt}}
+    else:
+        if usr in wklylb:
+            wklylb[usr]["code"]+=code
+            wklylb[usr]["crypt"]+=crypt
+        else:
+            wklylb[usr] = {"code": code, "crypt": crypt}
+    
+    obj = json.dumps(wklylb, indent = 4)
+    with open("assets/weekly_leaderboard.json", "w") as f:
+        f.write(obj)
+    f.close()
     
 
 bot.run(TOKEN)
