583416874a8d6de2b04aa31eaba7eb5f4b26017b
Stealth-py <generalom1234@gmail.com>
2021-06-13 16:08:08 +0530

    mmh

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import discord, os
-from discord import user
 from discord.ext import commands
+from discord.ext.commands import context
 from dotenv import load_dotenv
 import json
 from datetime import datetime
@@ -43,6 +43,8 @@ async def on_time():
                 users.append(":second_place: "+each[0])
             elif c==2:
                 users.append(":third_place: "+each[0])
+            else:
+                users.append(each[0])
             crypt.append(each[1])
             code.append(each[2])
             total.append(each[3])
@@ -56,35 +58,67 @@ async def on_time():
         embs = []
         e1 = discord.Embed(title="Weekly Leaderboard", color = 0xFFD700)
         if c<=5:
-            e1.add_field(name = "Username", value="\n".join(users), inline = True)
-            e1.add_field(name = "Cryptic Score", value = "\n".join(crypt), inline = True)
-            e1.add_field(name = "Comdimg Score", value = "\n".join(code), inline = True)
-            e1.add_field(name = "Total Score", value = "\n".join(total), inline = True)
+            e1.add_field(name = "Username", value="\n".join(map(str, users)), inline = True)
+            e1.add_field(name = "Cryptic Score", value = "\n".join(map(str, crypt)), inline = True)
+            e1.add_field(name = "Comdimg Score", value = "\n".join(map(str, code)), inline = True)
         else:
-            e1.add_field(name = "Username", value="\n".join(users[:5]), inline = True)
-            e1.add_field(name = "Cryptic Score", value = "\n".join(crypt[:5]), inline = True)
-            e1.add_field(name = "Comdimg Score", value = "\n".join(code[:5]), inline = True)
-            e1.add_field(name = "Total Score", value = "\n".join(total[:5]), inline = True)
+            e1.add_field(name = "Username", value="\n".join(map(str, users[:5])), inline = True)
+            e1.add_field(name = "Cryptic Score", value = "\n".join(map(str, crypt[:5])), inline = True)
+            e1.add_field(name = "Comdimg Score", value = "\n".join(map(str, code[:5])), inline = True)
         e1.set_footer(text = "Page 1", icon_url="https://cdn.discordapp.com/emojis/852073834337140756.png?v=1")
-        e1.set_author(name = "Made with <3 by Stealth.py", url="https://github.com/Stealth-py/", icon_url="https://cdn.discordapp.com/avatars/389111703725539328/a_ad503cedb63402fa14d4b48e3e5e77ef.gif?size=256&f=.gif")
+        e1.set_author(name = bot.user.display_name, icon_url="https://cdn.discordapp.com/emojis/852073834337140756.png?v=1")
         embs.append(e1)
         i = 5
-        for pg in range(2, pages+1):
+        for pg in range(1, pages):
             e = discord.Embed(title = "Weekly Leaderboard", color = 0xFFD700)
-            if pg!=pages:
-                e.add_field(name = "Username", value="\n".join(users[i:i+5]), inline = True)
-                e.add_field(name = "Cryptic Score", value = "\n".join(crypt[i:i+5]), inline = True)
-                e.add_field(name = "Comdimg Score", value = "\n".join(code[i:i+5]), inline = True)
-                e.add_field(name = "Total Score", value = "\n".join(total[i:i+5]), inline = True)
+            if pg!=pages-1:
+                e.add_field(name = "Username", value="\n".join(map(str, users[i:i+5])), inline = True)
+                e.add_field(name = "Cryptic Score", value = "\n".join(map(str, crypt[i:i+5])), inline = True)
+                e.add_field(name = "Comdimg Score", value = "\n".join(map(str, code[i:i+5])), inline = True)
             else:
-                e.add_field(name = "Username", value="\n".join(users[i:]), inline = True)
-                e.add_field(name = "Cryptic Score", value = "\n".join(crypt[i:]), inline = True)
-                e.add_field(name = "Comdimg Score", value = "\n".join(code[i:]), inline = True)
-                e.add_field(name = "Total Score", value = "\n".join(total[i:]), inline = True)
+                e.add_field(name = "Username", value="\n".join(map(str, users[i:])), inline = True)
+                e.add_field(name = "Cryptic Score", value = "\n".join(map(str, crypt[i:])), inline = True)
+                e.add_field(name = "Comdimg Score", value = "\n".join(map(str, code[i:])), inline = True)
             e.set_footer(text = f"Page {pg}", icon_url="https://cdn.discordapp.com/emojis/852073834337140756.png?v=1")
-            e.set_author(name = "Made with <3 by Stealth.py", url="https://github.com/Stealth-py/", icon_url="https://cdn.discordapp.com/avatars/389111703725539328/a_ad503cedb63402fa14d4b48e3e5e77ef.gif?size=256&f=.gif")
+            e.set_author(name = bot.user.display_name, icon_url="https://cdn.discordapp.com/emojis/852073834337140756.png?v=1")
             embs.append(e)
             i+=5
+        i = 0
+        msg = await ch.send(embed = e1)
+        await msg.add_reaction('⏮')
+        await msg.add_reaction('◀')
+        await msg.add_reaction('▶')
+        await msg.add_reaction('⏭')
+        print(embs, type(msg), pages, users)
+        emoji = ''
+        while 1:
+            if emoji=="\u23ee":
+                i = 0
+                await msg.edit(embed = embs[i])
+            if emoji == "\u25c0":
+                if i>0:
+                    i-=1
+                await msg.edit(embed = embs[i])
+            if emoji == "\u25b6":
+                if i<pages-1:
+                    i+=1
+                await msg.edit(embed = embs[i])
+            if emoji == "\u23ed":
+                i = pages-1
+                await msg.edit(embed = embs[i])
+        
+            flag, u = await bot.wait_for('reaction_add', check=lambda r, us: (r.emoji == '⏮' or r.emoji == '◀' or r.emoji == '▶' or r.emoji == '⏭' and r.message.id == msg.id) and msg.id == r.message.id and r.message.guild.id == msg.guild.id)
+            if flag is None:
+                break
+            if u.id != bot.user.id:
+                emoji = str(flag.emoji)
+                await msg.remove_reaction(flag.emoji, u)
+        await msg.clear_reactions()
+    wklylb = {}
+    obj = json.dumps(wklylb, indent = 4)
+    with open("assets/weekly_leaderboard.json", "w") as f:
+        f.write(obj)
+    f.close()
 ########
 
 @bot.command()
@@ -151,6 +185,98 @@ async def updatelb(ctx):
     with open("assets/weekly_leaderboard.json", "w") as f:
         f.write(obj)
     f.close()
+
+@bot.command()
+async def showlb(ctx):
+    ch = bot.get_channel(lb_id)
+    wkly = {}
+    with open("assets/weekly_leaderboard.json", "r") as f:
+        wkly = json.load(f)
+    f.close()
+    users, crypt, code = [], [], []
+    total = []
+    finalsc = []
+    for usr in wkly:
+        finalsc.append([usr, wkly[usr]["crypt"], wkly[usr]["code"], wkly[usr]["code"]+wkly[usr]["crypt"]])
+    finalsc = sorted(finalsc, reverse = True, key = lambda x: x[3])
+    c = 0
+    for each in finalsc:
+        if c==0:
+            users.append(":first_place: "+each[0])
+        elif c==1:
+            users.append(":second_place: "+each[0])
+        elif c==2:
+            users.append(":third_place: "+each[0])
+        else:
+            users.append(each[0])
+        crypt.append(each[1])
+        code.append(each[2])
+        total.append(each[3])
+        c+=1
+
+    tot = len(wkly)
+    pages = tot//5 + 1
+    if tot%5==0:
+        pages-=1
+
+    embs = []
+    e1 = discord.Embed(title="Weekly Leaderboard", color = 0xFFD700)
+    if c<=5:
+        e1.add_field(name = "Username", value="\n".join(map(str, users)), inline = True)
+        e1.add_field(name = "Cryptic Score", value = "\n".join(map(str, crypt)), inline = True)
+        e1.add_field(name = "Comdimg Score", value = "\n".join(map(str, code)), inline = True)
+    else:
+        e1.add_field(name = "Username", value="\n".join(map(str, users[:5])), inline = True)
+        e1.add_field(name = "Cryptic Score", value = "\n".join(map(str, crypt[:5])), inline = True)
+        e1.add_field(name = "Comdimg Score", value = "\n".join(map(str, code[:5])), inline = True)
+    e1.set_footer(text = "Page 1", icon_url="https://cdn.discordapp.com/emojis/852073834337140756.png?v=1")
+    e1.set_author(name = bot.user.display_name, icon_url="https://cdn.discordapp.com/emojis/852073834337140756.png?v=1")
+    embs.append(e1)
+    i = 5
+    for pg in range(1, pages):
+        e = discord.Embed(title = "Weekly Leaderboard", color = 0xFFD700)
+        if pg!=pages-1:
+            e.add_field(name = "Username", value="\n".join(map(str, users[i:i+5])), inline = True)
+            e.add_field(name = "Cryptic Score", value = "\n".join(map(str, crypt[i:i+5])), inline = True)
+            e.add_field(name = "Comdimg Score", value = "\n".join(map(str, code[i:i+5])), inline = True)
+        else:
+            e.add_field(name = "Username", value="\n".join(map(str, users[i:])), inline = True)
+            e.add_field(name = "Cryptic Score", value = "\n".join(map(str, crypt[i:])), inline = True)
+            e.add_field(name = "Comdimg Score", value = "\n".join(map(str, code[i:])), inline = True)
+        e.set_footer(text = f"Page {pg}", icon_url="https://cdn.discordapp.com/emojis/852073834337140756.png?v=1")
+        e.set_author(name = bot.user.display_name, icon_url="https://cdn.discordapp.com/emojis/852073834337140756.png?v=1")
+        embs.append(e)
+        i+=5
+    i = 0
+    msg = await ch.send(embed = e1)
+    await msg.add_reaction('⏮')
+    await msg.add_reaction('◀')
+    await msg.add_reaction('▶')
+    await msg.add_reaction('⏭')
+    print(embs, type(msg), pages, users)
+    emoji = ''
+    while 1:
+        if emoji=="\u23ee":
+            i = 0
+            await msg.edit(embed = embs[i])
+        if emoji == "\u25c0":
+            if i>0:
+                i-=1
+            await msg.edit(embed = embs[i])
+        if emoji == "\u25b6":
+            if i<pages-1:
+                i+=1
+            await msg.edit(embed = embs[i])
+        if emoji == "\u23ed":
+            i = pages-1
+            await msg.edit(embed = embs[i])
     
+        flag, u = await bot.wait_for('reaction_add', check=lambda r, us: (r.emoji == '⏮' or r.emoji == '◀' or r.emoji == '▶' or r.emoji == '⏭' and r.message.id == msg.id) and msg.id == r.message.id and r.message.guild.id == msg.guild.id)
+        if flag is None:
+            break
+        if u.id != bot.user.id:
+            emoji = str(flag.emoji)
+            await msg.remove_reaction(flag.emoji, u)
+    await msg.clear_reactions()
 
 bot.run(TOKEN)
